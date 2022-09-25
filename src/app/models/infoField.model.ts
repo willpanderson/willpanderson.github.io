c3e42d49bf42893bfcbbd7b3a0aa36f49ebf704b
William P. Anderson <william.anderson@mavs.uta.edu>
2022-09-25 14:46:39 -0500

    Delete src directory

--- src/app/models/infoField.model.ts
+++
@@ -1,3 +0,0 @@
-export class InfoField {
-  constructor(public field_1: string, public field_2: string, public field_3: string, public href: string) {}
-}
--- src/app/pages/contact/contact.model.ts
+++
@@ -1,3 +0,0 @@
-export class Contact {
-  constructor(public name: string, public href: string, public app: string, public icon: string) {}
-}
